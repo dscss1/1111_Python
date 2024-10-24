9930e1ceb949a16009dd9624d51bfebecfda2922
gololobov <gololobov.serhiy@gmail.com>
2024-10-24 17:51:31 +0300

    24.10.2024

--- Lesson 2.py
+++ Lesson 2.py
@@ -1,13 +1,14 @@
+import random
+
 
 def first():
     print("Hello")
     return 10
 
 
-def printLine():
-    for i in range(40):
-        print("*", end='')
-    print()
+def printLine(count, symbol):
+    print(symbol*count)
+
 
 def summ(var1, var2=0, var3=0, var4=0):
     result = var1 + var2 + var3 + var4
@@ -37,13 +38,29 @@ def isEven(number):
         return False
 
 
-a = [1,2,4,7,8,7,5,3,4,6]
+# a = [1,2,4,7,8,7,5,3,4,6]
+# count = 0
+# for i in a:
+#     if isEven(i):
+#         print(i, end=' ')
+#         count += 1
+#
+# print()
+# print(count)
+
+
+def orel_reshka():
+    n = random.randint(0,1)
+    if n == 1:
+        print("Орел")
+        return 1
+    else:
+        print("Решка")
+        return 0
+
 count = 0
-for i in a:
-    if isEven(i):
-        print(i, end=' ')
+for i in range(10):
+    if orel_reshka() == 1:
         count += 1
-
-print()
+printLine(5, '#=')
 print(count)
-
