9f57d8a208bef058f05946c62dffaa34af7d191d
dscss2 <euser6256@gmail.com>
2024-10-29 17:42:16 +0300

    Добавлено ДЗ 1

---
+++ DZ1/main.py
@@ -0,0 +1,16 @@
+import turtle
+
+def main():
+    turtle.shape("turtle")
+    turtle.pensize(10)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.penup()
+    turtle.color("red", "yellow")
+    turtle.pendown()
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(180)
+    turtle.forward(150)
+    turtle.done()
